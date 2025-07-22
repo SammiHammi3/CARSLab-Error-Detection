1c89c9a25cceba76a1c59d5327296954955f76e0
SammiHammi3 <swiesler05@gmail.com>
2025-07-22 16:54:54 -0400

    Add files via upload

--- src/lidar_error_checker/lidar_error_checker/pointcloud_checker.py
+++ src/lidar_error_checker/lidar_error_checker/pointcloud_checker.py
@@ -1,27 +1,28 @@
 import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import PointCloud2
+from rclpy.node import Node                     # use this to tell ROS2 that this is a ROS2 node
+from sensor_msgs.msg import PointCloud2         # needed for interpreting the pointcloud messages
 import sys
-import math
+import math     #used for IsNaN / IsInf
 from rclpy.qos import qos_profile_sensor_data
-from sensor_msgs_py import point_cloud2
-import hashlib
+from sensor_msgs_py import point_cloud2         # needed for interpreting the pointcloud messages
+import hashlib                                  # used to compare messages to previous messages
 
-from lidar_interfaces.msg import LidarAlert
+from lidar_interfaces.msg import LidarAlert     # From [the name of the package] import [the name of the message type]
 
 sys.stdout.reconfigure(line_buffering=True)
 
 # ideal values for the pointcloud / parameters for the pointcloud
-ideal_point_count = 230400  # ideal point count for one message
-ideal_message_rate = 10 # ideal frequency, in hz
-maximum_range = 200 # maximum range, in meters, according to the Pandar website
+ideal_point_count = 230400                      # ideal point count for one message
+ideal_message_rate = 10                         # ideal frequency, in hz
+maximum_range = 200                             # maximum range, in meters, according to the Pandar website
+
 
 # how far from expected values can we deviate?
 # note that 0.01 means 1%, not 0.01% (for example)
-percent_point_count_deviation_allowed = 0.001 # % deviation allowed from the ideal point count
-percent_invalid_point_count_allowed = 0.001 # what portion of points can be invalid before we flag the message
-percent_exceptional_point_count_allowed = 0.01 # what portion of points can be outside the maximum range before we flag the message
-percent_message_rate_deviation_allowed = 0.2 # 20% of the ideal hz in either direction before flag
+percent_point_count_deviation_allowed = 0.001   # % deviation allowed from the ideal point count
+percent_invalid_point_count_allowed = 0.001     # what portion of points can be invalid before we flag the message
+percent_exceptional_point_count_allowed = 0.01  # what portion of points can be outside the maximum range before we flag the message
+percent_message_rate_deviation_allowed = 0.2    # 20% of the ideal hz in either direction before flag
 
 
 
@@ -39,17 +40,17 @@ class PointCloudChecker(Node):
             self.listener_callback,
             qos_profile_sensor_data
 )
-        self.subscription  # prevent unused variable warning'
-        self.publisher = self.create_publisher(LidarAlert, 'LidarLogs', 10)
+        self.subscription  
+        self.publisher = self.create_publisher(LidarAlert, 'LidarLogs', 10)     # Message Type, name of topic to send to, queue depth
         self.prev_msg_time = None
         self.prev_data_hash = None
 
     def listener_callback(self, msg):
-        #Map for RVIZ2: velodyne_top_base_link
         self.check_data(msg)
         pass
 
     def check_data(self, msg):
+
         # Hierarchy of priority:
         # What is the frequency?
         # Is the message empty?
@@ -58,6 +59,7 @@ class PointCloudChecker(Node):
         # Are the points valid?
         # Are the points within the expected range?
 
+        # Update the current hash, time, and number of points before anything else
         current_hash = hashlib.md5(msg.data).hexdigest()
         msg_time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
         num_points = msg.width * msg.height     # number of points per ring * number of rings
@@ -66,27 +68,47 @@ class PointCloudChecker(Node):
         if self.prev_msg_time is not None:
             time_diff = msg_time - self.prev_msg_time
             missed_messages = round(time_diff * ideal_message_rate) -1 if time_diff > 0 else 0
-            frequency = (1+missed_messages) / time_diff if time_diff > 0 else 0
+            frequency = (1+missed_messages) / time_diff if time_diff > 0 else 0     # Number of messages / time difference = frequency
 
-            if abs(frequency-ideal_message_rate) > ideal_message_rate * percent_message_rate_deviation_allowed:
+            # If the frequency delta is more than the delta allowed
+            if abs(frequency-ideal_message_rate) > ideal_message_rate * percent_message_rate_deviation_allowed: #
                 self.publisher.publish(LidarAlert(
-                level=b"2", # ERROR level
+                level=b"3", # ERROR level
                 error_name="Message Rate Deviation",
                 description=f"Expected ~{ideal_message_rate:.2f}Hz, got {frequency:.2f}Hz"
+                ))        
+
+            if missed_messages > 0 and missed_messages < 10:
+                self.publisher.publish(LidarAlert(
+                level=b"2", # WARN level
+                error_name="Missed Messages",
+                description=f"This node missed {missed_messages} messages in a row. Maybe just a hiccup."
                 ))
+            if missed_messages>=10:
+                self.publisher.publish(LidarAlert(
+                level=b"3", # ERROR level
+                error_name="Missed Many Messages",
+                description=f"This node missed {missed_messages} messages in a row, around {missed_messages * 0.1} seconds. This is a serious issue."
+                ))
+
+        # Save the current time as the new "past" time
         self.prev_msg_time = msg_time
         
         # we want to filter the message so if it's empty or identical, we don't go through the whole process.
+        # FATAL error if no points are found
         if num_points == 0:
             self.publisher.publish(LidarAlert(
-                level=b"3", # FATAL level
+                level=b"4", # FATAL level
                 error_name="Empty Pointcloud",
                 description=f"Received PointCloud2 message with zero points"
             ))
             return
+
+        # Hashes the current and previous message, compares them.
+        # Error if identical -- almost 100% likely that a glitch has happened    
         if self.prev_data_hash == current_hash:
             self.publisher.publish(LidarAlert(
-            level=b"2", # ERROR level
+            level=b"3", # ERROR level
             error_name="Identical Frame",
             description=f"Received a frame identical to the previous"
             ))
@@ -95,14 +117,14 @@ class PointCloudChecker(Node):
         
         if num_points < min_points:
             self.publisher.publish(LidarAlert(
-            level=b"2", # ERROR level
+            level=b"3", # ERROR level
             error_name="Low Point Count",
             description=f"Received {num_points} points, below expected minimum of {min_points}"
             ))
                 
         if num_points > max_points:
             self.publisher.publish(LidarAlert(
-            level=b"1", # WARN level
+            level=b"2", # WARN level
             error_name="High Point Count",
             description=f"Received {num_points} points, above expected maximum of {max_points}"
             ))
@@ -118,7 +140,7 @@ class PointCloudChecker(Node):
             if not all(math.isfinite(v) for v in (x, y, z)):
                 invalid_count += 1
                 
-            # Check if the point is within the maximum range
+            # Check if the point is within the maximum range using pythagorean theorem
             distance = math.sqrt(x*x + y*y + z*z)
             if distance > maximum_range:
                 exceptional_count += 1
@@ -127,15 +149,15 @@ class PointCloudChecker(Node):
                 # If the ratio of exceptional to total is greater than the percent allowed, we publish a warning
         if (exceptional_count / num_points) > percent_exceptional_point_count_allowed:
                 self.publisher.publish(LidarAlert(
-                level=b"1", # WARN level
+                level=b"2", # WARN level
                 error_name="Exceptional Point Count",
                 description=f"Received {exceptional_count} points outside the maximum range of {maximum_range}."
                 ))
                     
-                # If the ratio of invalid to total is greater than the percent allowed, we publish a fatal alert
+                # If the ratio of invalid to total is greater than the percent allowed, we publish an error alert
         if (invalid_count / num_points) > percent_invalid_point_count_allowed:
                 self.publisher.publish(LidarAlert(
-                level=b"3", # FATAL level
+                level=b"3", # ERROR level
                 error_name="Invalid Point Count",
                 description=f"Received {invalid_count} invalid points"
                 ))
@@ -152,4 +174,3 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
     print("PointCloudChecker Node has been shut down.")
-    pass
